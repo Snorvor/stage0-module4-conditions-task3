5ae896392a69447e447c5b19d67645e30c54f519
Snorvor <bulezar@gmail.com>
2025-07-27 15:34:12 -0300

    calculate salary after to taxes

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary > 20000) {
+            salary = 20 * salary / 100;
+            System.out.println(salary);
+        } else if (salary > 10000) {
+            salary = 18 * salary / 100;
+            System.out.println(salary);
+        } else if (salary > 0 && salary < 10000) {
+            salary = 15 * salary / 100;
+            System.out.println(salary);
+        } else {
+            System.out.println("wrong input");
+        }
     }
 }
