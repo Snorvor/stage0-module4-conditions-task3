0caffe243838c5dababad3aca96e9b6dc207d28f
Snorvor <bulezar@gmail.com>
2025-07-27 17:29:35 -0300

    determinate if a number belongs to a month

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        if (month >= 1 && month <= 31) {
+            System.out.println(month);
+        } else {
+            System.out.println("wrong number");
+        }
     }
 }
